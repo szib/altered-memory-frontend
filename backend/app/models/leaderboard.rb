5259bd40e600905f460117af5e9c1c785780d6d0
Ivan Szebenszki <szib77@gmail.com>
2019-05-30 21:39:09 +0100

    Remove backend

--- backend/app/models/leaderboard.rb
+++
@@ -1,5 +0,0 @@
-class Leaderboard < ApplicationRecord
-  validates :name, presence: true
-  validates :score, presence: true
-  validates :time, presence: true
-end
--- backend/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  get '/leaderboards' => 'leaderboards#index'
-  post '/leaderboards' => 'leaderboards#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
