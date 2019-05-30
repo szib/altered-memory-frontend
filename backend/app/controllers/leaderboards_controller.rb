a96147cc6600bc88a07820fe387fce7527c85e77
Ivan Szebenszki <szib77@gmail.com>
2019-05-30 13:56:22 +0100

    Index: return top10 players

--- backend/app/controllers/leaderboards_controller.rb
+++ backend/app/controllers/leaderboards_controller.rb
@@ -1,7 +1,7 @@
 class LeaderboardsController < ApplicationController
 
     def index
-        @leaders = Leaderboard.all
+        @leaders = Leaderboard.all.sort_by(&:score).reverse.first(10)
         render json: @leaders, except: [:created_at, :updated_at]
     end
 
