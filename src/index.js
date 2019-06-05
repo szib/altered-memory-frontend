d1349b5a22eb692cfb2fd4cedcb8dd4aa849167c
Ivan Szebenszki <szib77@gmail.com>
2019-06-05 18:53:22 +0100

    Move files to no-cra-version and create-react-app

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
-// CSS
-// import './css/styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// JAVASCRIPT
-// import GameService from './js/stateMachine';
-// import * as UI from './js/UI';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   console.log('%cDOM loaded. Yaaayy...' + '%c¯\\_(ツ)_/¯', 'color: #0f0', 'color: yellow');
-//   const gameService = GameService.start();
-//   UI.startWith(gameService);
-// });
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-
-import styled from 'styled-components'
-import tw from 'tailwind.macro'
- 
-const Button = styled('button')`
-  ${tw`font-mono text-sm text-red-400 hover:text-blue-700`};
-`
-
-const screen = document.querySelector('#screen');
-
-ReactDOM.render(<Button>REACT</Button>, screen)
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
