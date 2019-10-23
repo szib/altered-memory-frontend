5ae786df1153366a6a51b37d24ad1ecd7a7eaaeb
Ivan Szebenszki <szib77@gmail.com>
2019-10-23 12:55:05 +0100

    Update backend url

--- src/js/UI/leaderboard.js
+++ src/js/UI/leaderboard.js
@@ -1,18 +1,19 @@
-import api from '../api';
+import api from "../api";
 
-const URL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:3000/results'
-  : 'https://memory-flatiron.herokuapp.com/results';
+const URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/results"
+    : "https://altered-memory-backend.herokuapp.com/results";
 
 const getLeaderboard = () => api(`${URL}/top/10`);
 
-export const postResult = (result) => {
+export const postResult = result => {
   const options = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify(result),
+    body: JSON.stringify(result)
   };
 
   console.warn(options);
@@ -21,9 +22,9 @@ export const postResult = (result) => {
     .catch(console.error);
 };
 
-const createRow = (user) => {
-  const div = document.createElement('div');
-  div.classList.add('leaderboard-item');
+const createRow = user => {
+  const div = document.createElement("div");
+  div.classList.add("leaderboard-item");
   div.innerHTML = `
     <div>${user.name}</div>
     <div>${user.score}</div>
@@ -32,18 +33,18 @@ const createRow = (user) => {
 };
 
 export const renderLeaderboard = () => {
-  const board = document.querySelector('#board');
-  board.classList = ['leaderboard'];
-  board.innerHTML = '';
+  const board = document.querySelector("#board");
+  board.classList = ["leaderboard"];
+  board.innerHTML = "";
   board.innerHTML = '<div class="text-orange-500 text-5xl">Leaderboard</div>';
   getLeaderboard()
-    .then((data) => {
-      data.forEach((user) => {
+    .then(data => {
+      data.forEach(user => {
         board.appendChild(createRow(user));
       });
     })
-    .catch((err) => {
-      const div = document.createElement('div');
+    .catch(err => {
+      const div = document.createElement("div");
       div.innerHTML = `
         <div class="w-full text-center text-4xl text-gray-100">No data from API. 😭</div>
         <small class="text-gray-700">${err}</small>
